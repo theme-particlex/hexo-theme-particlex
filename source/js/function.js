84a8b761256668420f437d47f80ac1aa5a96e919
argvchs <argvchs@outlook.com>
2022-12-19 10:03:09 +0800

    fix highlight

--- source/js/function.js
+++ source/js/function.js
@@ -5,7 +5,8 @@ function highlight() {
     hljs.highlightAll();
     let codes = document.getElementsByTagName("pre");
     for (let code of codes) {
-        const lang = [].filter.call(code.classList, lang => lang != "sourceCode")[0] || "text";
+        let langs = code.firstChild?.classList;
+        let lang = "text" || [].filter.call(langs, i => i != "sourceCode")[0];
         code.innerHTML = `<div class="code-content">${code.innerHTML}</div><div class="language">${lang}</div><div class="copycode"><i class="fa-solid fa-copy fa-fw"></i><i class="fa-solid fa-clone fa-fw"></i></div>`;
         let copycode = code.getElementsByClassName("copycode")[0];
         copycode.addEventListener("click", async function () {
