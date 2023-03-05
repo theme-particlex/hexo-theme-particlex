25873c376c09b6b901455329b732a77ccf61c340
argvchs <argvchs@outlook.com>
2023-03-05 22:24:19 +0800

    refactor: big refactoring

---
+++ source/js/lib/preview.js
@@ -0,0 +1,26 @@
+const previewMixin = {
+    data() {
+        return { previewShow: false };
+    },
+    methods: {
+        preview() {
+            let that = this;
+            let preview = this.$refs.preview,
+                content = this.$refs.previewContent,
+                images = document.getElementsByTagName("img");
+            for (let i of images)
+                i.addEventListener("click", function () {
+                    content.alt = this.alt;
+                    content.src = this.src;
+                    that.previewShow = true;
+                });
+            preview.addEventListener("click", () => {
+                this.previewShow = false;
+            });
+            window.addEventListener("resize", () => {
+                this.previewShow = false;
+            });
+        },
+    },
+};
+mixins.push(previewMixin);
---
+++ source/js/main.js
@@ -0,0 +1,47 @@
+const app = Vue.createApp({
+    mixins,
+    data() {
+        return {
+            loading: true,
+            showMenu: false,
+            barLocal: 0,
+        };
+    },
+    created() {
+        window.addEventListener("load", () => {
+            this.loading = false;
+        });
+    },
+    mounted() {
+        if (this.$refs.head) this.$refs.menu.classList.add("menu-color");
+        window.addEventListener("scroll", this.handleScroll, true);
+        this.render();
+    },
+    methods: {
+        homeClick() {
+            window.scrollTo({ top: window.innerHeight, behavior: "smooth" });
+        },
+        handleScroll() {
+            let menu = this.$refs.menu,
+                wrap = this.$refs.wrap;
+            let newlocal = document.documentElement.scrollTop;
+            if (this.barLocal < newlocal) {
+                this.showMenu = false;
+                menu.classList.add("hidden");
+            } else menu.classList.remove("hidden");
+            if (wrap) {
+                if (newlocal <= window.innerHeight - 100) menu.classList.add("menu-color");
+                else menu.classList.remove("menu-color");
+                if (newlocal <= 400) wrap.style.marginTop = -newlocal / 5 + "px";
+                else wrap.style.marginTop = "-80px";
+            }
+            this.barLocal = newlocal;
+        },
+        render() {
+            if (typeof this.highlight !== "undefined") this.highlight();
+            if (typeof this.math !== "undefined") this.math();
+            this.preview();
+        },
+    },
+});
+app.mount("#layout");
