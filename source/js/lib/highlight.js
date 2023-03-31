0fd40ea14aeea58b90942fac085371830ce09c4b
argvchs <argvchs@outlook.com>
2023-04-01 02:29:41 +0800

    refactor: format code

--- source/js/lib/highlight.js
+++ source/js/lib/highlight.js
@@ -10,23 +10,29 @@ mixins.highlight = {
     },
     methods: {
         highlight() {
-            let that = this;
             let codes = document.querySelectorAll("pre");
             for (let i of codes) {
-                let lang = [...i.classList, ...i.firstChild.classList][0] || "plaintext";
                 let code = i.innerText;
-                i.innerHTML = `<div class="code-content">${code}</div><div class="language">${lang}</div><div class="code-copy"><i class="fa-solid fa-copy fa-fw"></i><i class="fa-solid fa-clone fa-fw"></i></div>`;
-                let copy = i.querySelector(".code-copy");
-                copy.addEventListener("click", async function () {
-                    if (that.copying) return;
-                    that.copying = true;
-                    this.classList.add("copied");
+                let lang = [...i.classList, ...i.firstChild.classList][0] || "plaintext";
+                i.innerHTML = `
+                    <div class="code-content">${code}</div>
+                    <div class="language">${lang}</div>
+                    <div class="copycode">
+                        <i class="fa-solid fa-copy fa-fw"></i>
+                        <i class="fa-solid fa-clone fa-fw"></i>
+                    </div>
+                `;
+                let content = i.querySelector(".code-content");
+                let copycode = i.querySelector(".copycode");
+                copycode.addEventListener("click", async () => {
+                    if (this.copying) return;
+                    this.copying = true;
+                    copycode.classList.add("copied");
                     await navigator.clipboard.writeText(code);
                     await new Promise(resolve => setTimeout(resolve, 1000));
-                    this.classList.remove("copied");
-                    that.copying = false;
+                    copycode.classList.remove("copied");
+                    this.copying = false;
                 });
-                let content = i.querySelector(".code-content");
                 hljs.highlightElement(content);
             }
         },
