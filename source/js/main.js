683a181cbc2d4884825d1b5622c37ffd0ddf7c9a
YuziO2 <git@yuzi.dev>
2023-08-23 16:14:18 +0800

    feat: 黑暗模式
    在menu处增加了主题的切换选项，有浅色、深色和跟随系统三种选项。实现是借鉴tailwind的做法在根元素增加class='dark'来区别的。
    目前的缺陷：颜色我是比较随心的配的，作者可以根据喜好修改一下，建议把颜色放进:root里面，这样方便维护。
    另外评论区的颜色切换我只适配了waline一种。

--- source/js/main.js
+++ source/js/main.js
@@ -8,6 +8,7 @@ const app = Vue.createApp({
             menuColor: false,
             scrollTop: 0,
             renderers: [],
+            theme: localStorage.theme || "auto",
         };
     },
     created() {
@@ -38,6 +39,26 @@ const app = Vue.createApp({
                 else wrap.style.top = "-80px";
             }
             this.scrollTop = newScrollTop;
+        },
+        handleThemeSwitch() {
+            switch (this.theme) {
+                case "auto": //目前是自动，要切换成浅色
+                    document.documentElement.classList.remove("dark");
+                    localStorage.theme = "light";
+                    this.theme = "light";
+                    break;
+                case "light": //目前是浅色，要切换成深色
+                    document.documentElement.classList.add("dark");
+                    localStorage.theme = "dark";
+                    this.theme = "dark";
+                    break;
+                case "dark": //目前是深色，要切换成自动
+                    window.matchMedia("(prefers-color-scheme: dark)").matches || //系统不是深色模式
+                        document.documentElement.classList.remove("dark");
+                    localStorage.removeItem("theme");
+                    this.theme = "auto";
+                    break;
+            }
         },
     },
 });
