4b5932e2ef8fcb1cb3338bb7130d56cee2a0e3fc
argvchs <argvchs@outlook.com>
2023-08-23 21:02:57 +0800

    style: format code

--- source/js/main.js
+++ source/js/main.js
@@ -21,7 +21,6 @@ const app = Vue.createApp({
     },
     methods: {
         render() {
-            if (typeof this.renderers === "undefined") return;
             for (let i of this.renderers) i();
         },
         handleScroll() {
