8b1f7014594987d1013e449781b1f043c2f11283
SaltedFishes <66206988+SaltedFishesNG@users.noreply.github.com>
2024-03-19 15:27:43 +0800

    feat: Switch Dark Mode Button

--- source/js/lib/highlight.js
+++ source/js/lib/highlight.js
@@ -1,13 +1,3 @@
-let colorSchemeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-let highlightDarkstyle = document.querySelector("#highlight-darkstyle");
-if (colorSchemeMediaQuery.matches) { highlightDarkstyle.toggleAttribute("disabled"); }
-colorSchemeMediaQuery.addEventListener('change', e => {
-    if (e.matches) {
-        highlightDarkstyle.removeAttribute("disabled");
-    } else {
-        highlightDarkstyle.setAttribute("disabled", "");
-    }
-});
 mixins.highlight = {
     data() {
         return { copying: false };
